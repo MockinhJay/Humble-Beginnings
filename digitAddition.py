d7af62eeed17cedfed05ec5cb671d252b5d4b482
Mockingjay21 <shababhasnat@gmail.com>
2023-07-05 13:51:03 +0600

    Delete digitAddition.py

--- digitAddition.py
+++
@@ -1,7 +0,0 @@
-two_digit_number = input("Type a two digit number: ")
-
-firstDigit = two_digit_number[0]
-secondDigit = two_digit_number[1]
-
-result = int(firstDigit) + int(secondDigit)
-print(result)
